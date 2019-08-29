5d05f26e61b999b27fc7a65cce26588cd4eea2a3
tushain <54601062+tushain@users.noreply.github.com>
2019-08-29 17:10:04 -0400

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*Author: Tushain	
+Desc: Welcome message*/
+
+public class Welcome {
+
+
+public static void main (String[] args) {
+
+
+
+//Create a welcome message and store in variable
+String welcome = "Welcome to Java World";
+//Output the welcome message
+System.out.print(welcome);
+}
+
+}
+
+
+
+
