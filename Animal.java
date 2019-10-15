98bcedf96e75082c648ecadcdb8f6925865fb4d2
tushain <54601062+tushain@users.noreply.github.com>
2019-10-15 16:37:58 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+package week6;
+
+public class Animal {
+
+	public int numlegs;
+	private int age;
+	
+	public void bark()
+	{
+		System.out.println("Where my dogs at?");
+	}
+	public void run()
+	{
+	}
+	
+}
